ed11a6ff0498e932597469f54f3a562866ccbce5
Habib Royni Mujtahid <habib.sickh@gmail.com>
2020-02-22 00:30:33 +0700

    typo path

--- secrets/decrypt.sh
+++ secrets/decrypt.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/env.json env.json.gpg
+--output $HOME/secrets/env.json ./secrets/env.json.gpg
